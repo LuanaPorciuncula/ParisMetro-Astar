6821130ca6126d3795909816ea627951bdb9b810
Luana Porciuncula <lpb@cin.ufpe.br>
2021-07-23 16:37:48 -0300

    add current path of each node in the border

--- metro_de_paris.py
+++ metro_de_paris.py
@@ -1,14 +1,12 @@
 import pandas
 import math
 
+# Obtendo dados do problema
 dir_dist = pandas.read_csv('dir_dist.csv', header=None)
 real_dist = pandas.read_csv('real_dist.csv', header=None)
-lines = pandas.read_csv('lines.csv', header=None)
-    
+lines = pandas.read_csv('lines.csv', header=None)  
 # print(dir_dist)
-dir_dist = dist_km_to_minutes(dir_dist)
 # print(real_dist)
-real_dist = dist_km_to_minutes(real_dist)
 # print(lines)
 
 
@@ -16,10 +14,15 @@ def dist_km_to_minutes(dist):
     for i in range(14):
         for j in range(14):    
             # a velocidade média de um trem é de 30km/h -> 1km/2min
-            dist[i][j] = dist[i][j]*2
+            dist[i][j] = dist[i][j]*2.0
             
-    # print(dist)
     return dist
+
+# Convertendo dados do problema de km pra minutos 
+dir_dist = dist_km_to_minutes(dir_dist)
+real_dist = dist_km_to_minutes(real_dist)
+# print(dir_dist)
+# print(real_dist)
 
 
 def valid_station(str_station):
@@ -34,7 +37,8 @@ def valid_station(str_station):
 def get_station(req_station):
     is_valid = False
     while not is_valid:
-        print("Estações: E1 - E2 - E3 - E4 - E5 - E6 - E7 - E8 - E9 - E10 - E12 - E13 - E14")
+        print("Estações: E1 - E2 - E3 - E4 - E5 - E6 - E7",
+              "- E8 - E9 - E10 - E11 - E12 - E13 - E14")
         print(req_station)
         str_station = input()
 
@@ -56,6 +60,7 @@ def get_connected_stations(station, marked_stations):
     for con, dist in enumerate(real_dist[station]):
         if not math.isnan(dist):
             connected_stations.append(con)
+    # Remove estações que já foram percorridas no caminho
     connected_stations = list(
             filter(lambda station: station not in marked_stations, 
                    connected_stations))
@@ -73,7 +78,6 @@ def estimate_dist(target, destiny):
 def covered_dist(current, target, covered_path):
     c_dist = 0
     line = ""
-    
     if len(covered_path) > 0:
         line = lines[covered_path[0][0]][covered_path[0][1]]
     
@@ -96,18 +100,26 @@ def covered_dist(current, target, covered_path):
     line = newline
     return c_dist, line
 
-def expand_border(current, destiny, covered_path, border, marked_stations):
+def expand_border(current, destiny, border, covered_path, marked_stations):
     connected_stations = get_connected_stations(current, marked_stations)
-    # Se não tiver como seguir por um caminho, volte pro nó anterior
-    if len(connected_stations) == 0 and len(covered_path) > 0:
-        covered_path.pop()
     
     newborder = border
     for station in connected_stations:
+        # Calcula a função de avaliação do A*
         c_dist, curline = covered_dist(current, station, covered_path)
         e_dist = estimate_dist(station, destiny)
-        newborder.append([station, curline, c_dist+e_dist, c_dist, e_dist])
+        
+        f = round(c_dist + e_dist, 1)
+        g = round(c_dist, 1)
+        h = round(e_dist, 1)
+        
+        # Adiciona o caminho e as estações visitadas para cada nó na fronteira
+        new_path = covered_path + [[current, station]]
+        new_m_stations = marked_stations
+        new_m_stations.add(current)
+        newborder.append([station, curline, f, g, h, new_path, new_m_stations])
 
+    # Ordena pela função de avaliação
     newborder = sorted(newborder, key=lambda x: x[2])
     return newborder
 
@@ -121,38 +133,39 @@ def print_iter(border):
             g = str(e[3])
             h = str(e[4])
             it_border+= "("+station+" "+line+" "+ f +" "+ g + " "+ h + "), "
+            # print(e[5])
         print(it_border)
+        
 
  
-def astar(destiny, border, covered_path, marked_stations):
-    if border[0][0] != destiny:
-        if len(covered_path) == 0:
-            current = border[0][0]
-        else:
-            current = covered_path[-1][1]
+def astar(current, destiny, border):
+    # Obtém o caminho que foi percorrido até a estação atual
+    covered_path = border[0][5]
+    
+    if current != destiny:
+        marked_stations = border[0][6]
         border = border[1:]
         
-        border = expand_border(current, destiny, covered_path, border, marked_stations)
+        border = expand_border(current, destiny, border, 
+                                   covered_path, marked_stations)
         print_iter(border)
         
-        if(len(covered_path) == 0):
-            covered_path=[[current, border[0][0]]]
-        else:
-            covered_path.append([covered_path[-1][1], border[0][0]])
-        marked_stations.add(current)
-        astar(destiny, border, covered_path, marked_stations)
+        current = border[0][0]
+        astar(current, destiny, border)
     else:
         print("Trajeto mais rápido")
         fast_path = ""
         for ride in covered_path:
             fast_path+="E"+str(ride[0]+1)+" - "
+        
         fast_path += "E"+str(covered_path[-1][1]+1)
         
         print(fast_path)
-        #print("GG")
 
-
-border = [[s_station, "", dir_dist[s_station][d_station], 0, dir_dist[s_station][d_station]]]   
-covered_path = [] 
+f = dir_dist[s_station][d_station]
+g = 0
+h = dir_dist[s_station][d_station]
+covered_path = []
 marked_stations = set()
-astar(d_station, border, covered_path, marked_stations)
+border = [[s_station, "", f, g, h, covered_path, marked_stations]]
+astar(s_station, d_station, border)
